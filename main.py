0b5600de6f2f46bc0a9f543e60e630f26c0a35ea
Ricardo Franco dos Santos <ricardofrancodossantos@Ricardos-MacBook-Air.local>
2026-01-18 22:44:43 -0300

    feat: python programming logic first exercise: first input

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("ola") # type: ignore
+1. Which are all necessary data inputs?
+2. What i need to do with this data?
+3. Which are all restrictions of this problem?
+4. Which is the expected result?
+5. Which are the step sequence to achieve the expected result?
+
+
+monthly_salary = input ('Which is your monthly salary: ')

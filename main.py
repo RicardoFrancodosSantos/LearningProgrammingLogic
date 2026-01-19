528b2fe0a29d156f0238d7051cec228983b99d5e
Ricardo Franco dos Santos <ricardofrancodossantos@Ricardos-MacBook-Air.local>
2026-01-18 23:32:00 -0300

    feat: python programming logic exercise finalization

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-1. Which are all necessary data inputs?
-2. What i need to do with this data?
-3. Which are all restrictions of this problem?
-4. Which is the expected result?
-5. Which are the step sequence to achieve the expected result?
+"1. Which are all necessary data inputs?"
+"2. What i need to do with this data?"
+"3. Which are all restrictions of this problem?"
+"4. Which is the expected result?"
+"5. Which are the step sequence to achieve the expected result?"
 
 
 monthly_salary = input ('Which is your monthly salary: ')
 worked_hours = input ('How many hours you worked: ')
 hourly_value = float(monthly_salary) / int(worked_hours)
+print(hourly_value)

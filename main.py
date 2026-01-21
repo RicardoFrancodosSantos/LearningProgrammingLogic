259019bf926e5517b429a037bf1a45e4707ee834
Ricardo Franco dos Santos <ricardofrancodossantos@Ricardos-MacBook-Air.local>
2026-01-21 19:40:12 -0300

    feat: variables declaration

--- main.py
+++ main.py
@@ -4,21 +4,13 @@
 "4. Which is the expected result?"
 "5. Which are the step sequence to achieve the expected result?"
 
-"Conditionals if elif else"
 
-"I’m late for class. Can I still come in?"
 
-"If this were your first or second time, yes, you could. But if this were your third time, you would be suspended."
+"Problem: build an application that receive two values and show the biggest beetween then""
 
-lates = int(input('How many absences do you have?'))
-if lates >= 3:
-    print("you`re suspended")
-elif lates == 1:
-    print("you have just two more chances")
-elif lates == 2:
-    print("you have just one more chance")
-else:
-    print ("you didn`t late yet")
+first_value= int(input('Insert the first value: '))
+second_value= int(input('Insert the second value: '))
+
 
 
 

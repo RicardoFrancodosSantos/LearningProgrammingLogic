39aab636cd13569d861fd04e7568e7fa746d1ae8
Ricardo Franco dos Santos <ricardofrancodossantos@Ricardos-MacBook-Air.local>
2026-01-23 07:56:02 -0300

    feat: learning for with conditionals, improving the result

--- loops.py
+++ loops.py
@@ -5,4 +5,7 @@
 ages = [13,15,16,17,18,20,56,87]
 for age in ages:
         if age >= 18:
-            print(age)
+            print(f'{age} is of legal age')
+
+        else:
+              print(f'{age} is not of legal age')

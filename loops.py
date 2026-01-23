b4cf08c896f28eba783d65742c2512066022165d
Ricardo Franco dos Santos <ricardofrancodossantos@Ricardos-MacBook-Air.local>
2026-01-23 07:52:29 -0300

    feat: learning for with conditional

--- loops.py
+++ loops.py
@@ -1,5 +1,8 @@
 
-# name list
-names = ['John','Claudia', 'Thialla', 'Ricardo','Izzie']
-for name in names:
-    print(name)
+
+
+    # only show the legal ages in the list
+ages = [13,15,16,17,18,20,56,87]
+for age in ages:
+        if age >= 18:
+            print(age)

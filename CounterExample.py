7251031474ef558f960105d8ecad78eaa7f2a607
Ricardo Franco dos Santos <ricardofrancosantos@gmail.com>
2026-01-26 16:13:57 -0300

    feat: while programming logic with counter

---
+++ CounterExample.py
@@ -0,0 +1,11 @@
+#Counters inside a while loop.
+#Be notified at 5:00 PM about the sunset.
+#Count hour by hour until reaching 5:00 PM.
+#When it reaches 5:00 PM, display: “time to watch the sunset”.
+hour = 0
+while hour <= 5:
+    print(hour)
+    hour = hour +1
+
+
+print('time to see the twilight')

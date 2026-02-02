e30ce6d2cd620abd90311b6fcc1b3ab62b36dc58
Ricardo Franco dos Santos <ricardofrancosantos@gmail.com>
2026-02-02 15:53:25 -0300

    salary employee sum  exercise

--- lists.py
+++ lists.py
@@ -13,3 +13,9 @@
 #5.1.receive the salaries list
 #5.2.sum the salaries list until receive the total
 #5.3 print the value of the sum
+
+salaries = [2500, 900, 500, 7500]
+total = 0
+for salary in salaries:
+    total = total + salary
+    print(total)

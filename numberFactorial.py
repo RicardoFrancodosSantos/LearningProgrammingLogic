b0a9fd46b6b86184755472e5e49682703b05565a
Ricardo Franco dos Santos <ricardofrancosantos@gmail.com>
2026-02-02 16:30:39 -0300

    number factorial exercise fully

--- numberFactorial.py
+++ numberFactorial.py
@@ -23,3 +23,14 @@ a positive and integer number
 #3. identify the factorial value and multiply (initializing with 1 until the factorial value, executing sum the previous output until reach in the last calculus (trough the factorial value))
 #4. Print the output in the screen
 '''
+
+number = int(input ('Type desired factorial that you want to calculate: '))
+if number > 0 and type(number) == int:
+   factorial = 1
+   for item in range (1, number + 1): 
+    print(f'{factorial} * {item}')
+    factorial = factorial * item
+    print(f'{factorial}')
+   print(f' the factorial of the {number} is {factorial}') 
+else:
+  print ('please, only inform positive and integer numbers')

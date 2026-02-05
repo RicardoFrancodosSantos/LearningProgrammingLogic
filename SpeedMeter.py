cc1cca8693a4817c9c9f155db9ba67ebf8137756
Ricardo Franco dos Santos <ricardofrancosantos@gmail.com>
2026-02-05 10:26:13 -0300

    feat: feat: exercise description

---
+++ SpeedMeter.py
@@ -0,0 +1,12 @@
+#**Project 3: Speed Meter**
+
+'''Create a program that receives from the user a value representing the speed on a road where the maximum allowed speed is 80 km/h.
+
+Based on this value, the program must inform whether the driver received a minor, serious, or very serious fine.
+
+If the speed is less than or equal to 80 km/h, display: “no fine”.
+If it is up to 10 km/h over the limit, display: “received a minor fine”.
+If it is between 11 km/h and 20 km/h over the limit, display: “received a serious fine”.
+If it is more than 20 km/h over the limit, display: “received a very serious fine”.'''
+#
+
